f4db808debe809bdb7fd607f072ee26a4ce8bca3
Hassan Naqvi <shznaqvi@gmail.com>
2016-12-29 16:19:21 +0500

    New changelist

---
+++ app/src/main/java/edu/aku/hassannaqvi/mnch_src/Section7IMActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.mnch_src;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Section7IMActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section7_im);
+    }
+}
