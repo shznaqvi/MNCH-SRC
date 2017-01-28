e2315be50fee7191cec557dce97b453fb05e9add
gul.sanober <gulsanobermemon@gmail.com>
2017-01-28 17:18:44 +0500

    Section 5A Skip Pattern

--- app/src/main/java/edu/aku/hassannaqvi/mnch_src/MainActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/mnch_src/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
 
     public void OpenSection4(View v) {
 
-        Intent sec1_intent = new Intent(this, Section4bActivity.class);
+        Intent sec1_intent = new Intent(this, Section5Activity.class);
         startActivity(sec1_intent);
 
     }
